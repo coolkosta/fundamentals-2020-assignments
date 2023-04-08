ee6f3d819e838a835cb67af87a69bcaff6175a83
coolkosta <kostia.borzakov@gmail.com>
2023-04-08 16:39:25 +0300

    commot WS 1-3 litle 4

--- app/src/main/java/com/android/academy/fundamentals/workshop_1/WS01ThreadsTaskFragment.kt
+++ app/src/main/java/com/android/academy/fundamentals/workshop_1/WS01ThreadsTaskFragment.kt
@@ -1,6 +1,10 @@
 package com.android.academy.fundamentals.workshop_1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -12,7 +16,7 @@ class WS01ThreadsTaskFragment: Fragment(R.layout.fragment_ws_01) {
     private var threadButton : Button? = null
     private var threadTextView : TextView? = null
 
-    //TODO(WS1:2) Create a private val handler and set yours
+    val handler = MyHandler()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         findViews(view)
@@ -34,11 +38,15 @@ class WS01ThreadsTaskFragment: Fragment(R.layout.fragment_ws_01) {
     }
 
     private fun startThread() {
+        val thread = MyThread()
+        thread.start()
         printMessage(getString(R.string.wait))
         //TODO(WS1:5) create your thread and start it
     }
 
     private fun startRunnable() {
+        val thread = MyThread()
+        thread.start()
         printMessage(getString(R.string.wait))
         //TODO(WS1:8) create your runnable and start it
     }
@@ -49,6 +57,15 @@ class WS01ThreadsTaskFragment: Fragment(R.layout.fragment_ws_01) {
 
     //TODO(WS1:1) Create inner class Handler
     // example: MyHandler : Handler(Looper.getMainLooper())
+
+    @SuppressLint("HandlerLeak")
+    inner class MyHandler: Handler(Looper.getMainLooper()) {
+
+        override fun handleMessage(msg: Message) {
+            printMessage(msg.data.getString(MESSAGE_KEY, ""))
+        }
+
+    }
 
     //TODO(WS1:2) override function handleMessage(mes: Message)
     // at the function run printMessage
@@ -62,6 +79,15 @@ class WS01ThreadsTaskFragment: Fragment(R.layout.fragment_ws_01) {
     // create Message, put string to the message.data with MESSAGE_KEY
     // send message to the handler handler.sendMessage(mes)
 
+    inner class MyThread: Thread() {
+        override fun run() {
+            sleep(6000)
+            val msg = Message()
+            msg.data.putString(MESSAGE_KEY, getString(R.string.thread_worked))
+            handler.sendMessage(msg)
+        }
+    }
+
     //TODO(WS1:6) Create inner class Runnable
     // example:  MyRunnable : Runnable
     // override function run
@@ -69,6 +95,16 @@ class WS01ThreadsTaskFragment: Fragment(R.layout.fragment_ws_01) {
     //TODO(WS1:7) at the run function emulate long work with Thread.sleep(4000)
     // create Message, put string to the message.data with MESSAGE_KEY
     // send message to the handler handler.sendMessage(mes)
+
+    inner class MyRunnable: Runnable {
+        override fun run() {
+            Thread.sleep(4000)
+            val msg = Message()
+            msg.data.putString(MESSAGE_KEY, getString(R.string.runnable_worked))
+            handler.sendMessage(msg)
+        }
+
+    }
 
     companion object {
         private const val MESSAGE_KEY = "key"
--- app/src/main/java/com/android/academy/fundamentals/workshop_3/WS03CoroutinesProblemFragment.kt
+++ app/src/main/java/com/android/academy/fundamentals/workshop_3/WS03CoroutinesProblemFragment.kt
@@ -54,14 +54,28 @@ class WS03CoroutinesProblemFragment : Fragment(R.layout.fragment_ws_03) {
         toggleButtons(true)
 
         // TODO: WS_5_03_1 - Add run odd number producing coroutine via launch in scope
+        scope.launch {
+            runOddsCoroutine()
+        }
 
         // TODO: WS_5_03_2 - Add run negatives number producing coroutine via launch in scope
 
+        scope.launch {
+            runNegativesCoroutine()
+        }
+
         // TODO: WS_5_03_3 - Add run mod by two result from number coroutine via launch in scope
+        scope.launch {
+            runModByTwoCoroutine()
+        }
         // TODO: WS_5_03_7 - Add run mod by two result from number coroutine via launch in GlobalScope
         // TODO: Don't forget to save job for the future cancellation
+        globalScopeJob = GlobalScope.launch { runModByTwoCoroutine() }
 
         // TODO: WS_5_03_4 - Add run coroutine that fails after a second
+        scope.launch {
+            runCoroutineThatFails()
+        }
     }
 
     // Coroutine that produces odd numbers every second
@@ -102,15 +116,17 @@ class WS03CoroutinesProblemFragment : Fragment(R.layout.fragment_ws_03) {
         throw IllegalStateException("Some exception")
     }
 
-    private fun showResult(text: String, resultView: TextView?) {
+    private suspend fun showResult(text: String, resultView: TextView?) {
         // TODO: WS_5_03_6 - Fix access of UI component from main dispatcher
         // TODO: Don't forget about suspend keyword
-        resultView?.text = text
+        withContext(Dispatchers.Main) {
+            resultView?.text = text
+        }
     }
 
     private fun cancelCoroutines() {
         // TODO: WS_5_03_5 - Add cancellation of all current jobs via parent job
-
+        scope.cancel()
         // Set new scope with fresh SupervisorJob after cancel
         scope = CoroutineScope(SupervisorJob() + Dispatchers.Default + exceptionHandler)
 
@@ -139,6 +155,7 @@ class WS03CoroutinesProblemFragment : Fragment(R.layout.fragment_ws_03) {
     override fun onDestroyView() {
         // TODO: WS_5_03_8 - Add Global Scope job cancelling to avoid leaks
         // TODO: use WS03CoroutinesProblemFragment#globalScopeJob
+        globalScopeJob?.cancel()
 
         // TODO: WS_5_03_11 - Stop jobs to avoid leaks
 
